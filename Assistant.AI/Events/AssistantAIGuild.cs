64b377f2fe60e00d21dc63b903f20a67a793fd21
AALUND13 <tamlund1976@gmail.com>
2024-09-27 04:23:22 -0400

    Move `AssistantAIGuild` tools function into `AssistantAIGuildTools` file.

--- Assistant.AI/Events/AssistantAIGuild.cs
+++ Assistant.AI/Events/AssistantAIGuild.cs
@@ -99,23 +99,6 @@ public partial class AssistantAIGuild : IEventHandler<MessageCreatedEventArgs>, 
         LoadMessagesFromDatabase();
     }
 
-    [Description("Join the voice channel of a user.")]
-    void JoinUserVC([Description("The user ID to join the voice channel of.")] ulong userID) {
-        DiscordChannel? channel = (client.Guilds.Values.SelectMany(guild => guild.VoiceStates.Values)
-            .FirstOrDefault(voiceState => voiceState.User.Id == userID)?.Channel)
-            ?? throw new ArgumentException($"User with ID {userID} is not in a voice channel.");
-
-        channel.ConnectAsync().Wait();
-    }
-
-    [Description("Get information about a user.")]
-    string GetUserInfo([Description("The user ID to get information about.")] ulong userID) {
-        DiscordUser? user = client.GetUserAsync(userID).Result
-            ?? throw new ArgumentException($"User with ID {userID} was not found.");
-
-        return $"[User: {user.GlobalName ?? user.Username} | ID: {user.Id}]";
-    }
-
     public async Task HandleEventAsync(DiscordClient sender, MessageCreatedEventArgs eventArgs) {
         if(eventArgs.Author.IsBot // Check if the author is a bot
             || eventArgs.Channel.IsPrivate // Check if the channel is a direct message
