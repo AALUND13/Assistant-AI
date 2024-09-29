1525cb540241e0477db61d422c235cce8c9d752d
AALUND13 <tamlund1976@gmail.com>
2024-09-29 02:44:16 -0400

    Added Status and Custom Status to `GetUserInfo`.

--- Assistant.AI/Events/Tools/AssistantAIGuildTools.cs
+++ Assistant.AI/Events/Tools/AssistantAIGuildTools.cs
@@ -1,14 +1,24 @@
 ﻿using DSharpPlus.Entities;
 using System.ComponentModel;
+using System.Text;
 
 namespace AssistantAI.Events {
     public partial class AssistantAIGuild {
         [Description("Get information about a user.")]
         string GetUserInfo([Description("The user ID to get information about.")] ulong userID) {
-            DiscordUser? user = client.GetUserAsync(userID).Result
+            DiscordUser user = client.GetUserAsync(userID).Result
                 ?? throw new ArgumentException($"User with ID {userID} was not found.");
 
-            return $"[User: {user.GlobalName ?? user.Username} | ID: {user.Id}]";
+            string? customActivity = user.Presence?.Activities.FirstOrDefault(activity => activity.ActivityType == DiscordActivityType.Custom)?.RichPresence.State;
+
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append($"User: {user.GlobalName ?? user.Username}");
+            stringBuilder.Append($" | Status: {Enum.GetName(user.Presence?.Status ?? DiscordUserStatus.Offline)}");
+            if(customActivity != null)
+                stringBuilder.Append($" | Custom Activity: {customActivity}");
+
+            
+            return $"[{stringBuilder}]";
         }
     }
 }
