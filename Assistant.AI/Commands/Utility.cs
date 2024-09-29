19f4c75ef814e8fd31bdf0e9a2f8db18b09328f3
AALUND13 <tamlund1976@gmail.com>
2024-09-28 20:20:15 -0400

    Remove VC command and tools function.

--- Assistant.AI/Commands/Utility.cs
+++ Assistant.AI/Commands/Utility.cs
@@ -15,15 +15,4 @@ public class Utility {
     public static ValueTask PingAsync(CommandContext ctx) {
         return ctx.ResponeTryEphemeral($"Pong! **{(int)ctx.Client.GetConnectionLatency(ctx.Guild?.Id ?? ctx.Client.Guilds[0].Id).TotalMilliseconds}ms**", true);
     }
-
-#if DEBUG
-    [Command("join-vc")]
-    [Description("Join the voice channel you are in.")]
-    [Cooldown(5)]
-    public async ValueTask JoinVcAsync(SlashCommandContext ctx, DiscordChannel? channel = null) {
-        channel ??= ctx.Member!.VoiceState?.Channel;
-        await channel!.ConnectAsync();
-        await ctx.ResponeTryEphemeral($"Joined {channel!.Mention}.", true);
-    }
-#endif
 }
--- Assistant.AI/Events/Tools/AssistantAIGuildTools.cs
+++ Assistant.AI/Events/Tools/AssistantAIGuildTools.cs
@@ -4,15 +4,6 @@ using System.ComponentModel;
 
 namespace AssistantAI.Events {
     public partial class AssistantAIGuild {
-        [Description("Join the voice channel of a user.")]
-        void JoinUserVC([Description("The user ID to join the voice channel of.")] ulong userID) {
-            DiscordChannel? channel = (client.Guilds.Values.SelectMany(guild => guild.VoiceStates.Values)
-                .FirstOrDefault(voiceState => voiceState.User.Id == userID)?.Channel)
-                ?? throw new ArgumentException($"User with ID {userID} is not in a voice channel.");
-
-            channel.ConnectAsync().Wait();
-        }
-
         [Description("Get information about a user.")]
         string GetUserInfo([Description("The user ID to get information about.")] ulong userID) {
             DiscordUser? user = client.GetUserAsync(userID).Result
