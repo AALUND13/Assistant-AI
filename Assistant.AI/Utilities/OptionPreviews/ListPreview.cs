dee66c9b1e7e8607eefa44e27128ec45e12d9982
AALUND13 <tamlund1976@gmail.com>
2024-10-11 07:09:18 -0400

    Integrate PreviewManager for custom previews and parsing in ListPreview.

--- Assistant.AI/Utilities/OptionPreviews/ListPreview.cs
+++ Assistant.AI/Utilities/OptionPreviews/ListPreview.cs
@@ -8,7 +8,7 @@ namespace AssistantAI.Utilities.OptionPreviews {
             int totalLength = 0;
             string resultString = "";
             foreach(T item in value) {
-                string itemAsString = item.ToString();
+                string itemAsString = PreviewManager.GetPreview<T>()?.GetPreview(item) ?? item?.ToString() ?? "Null";
                 if(totalLength + itemAsString.Length > 100) {
                     resultString += "...";
                     break;
@@ -17,38 +17,33 @@ namespace AssistantAI.Utilities.OptionPreviews {
                 totalLength += itemAsString.Length + 2;
             }
             return $"[{resultString.TrimEnd(", ".ToCharArray())}]";
-            
-
         }
 
         public (List<T>?, bool) Parse(string value) {
             List<string> result = value.Split(',').Select(v => v.Trim()).ToList();
+            Type type = typeof(T);
 
             bool canParse = result.All(v => {
-                Type type = typeof(T);
-                switch(type.Name) {
-                    case nameof(Int32):
-                        return int.TryParse(v, out _);
-                    case nameof(Int64):
-                        return long.TryParse(v, out _);
-                    case nameof(UInt32):
-                        return uint.TryParse(v, out _);
-                    case nameof(UInt64):
-                        return ulong.TryParse(v, out _);
-                    case nameof(Boolean):
-                        return bool.TryParse(v, out _);
-                    case nameof(String):
-                        return true;
-                    default:
-                        return false;
-                }
+                return type.Name switch {
+                    nameof(Int32) => int.TryParse(v, out _),
+                    nameof(Int64) => long.TryParse(v, out _),
+                    nameof(UInt32) => uint.TryParse(v, out _),
+                    nameof(UInt64) => ulong.TryParse(v, out _),
+                    nameof(Boolean) => bool.TryParse(v, out _),
+                    nameof(String) => true,
+                    _ => PreviewManager.GetPreview<T>()?.Parse(v).Item2 ?? false,
+                };
             });
 
             if(canParse) {
+                if(PreviewManager.GetPreview<T>()?.Parse(value).Item2 ?? false) {
+                    return (result.Select(v => PreviewManager.GetPreview<T>()!.Parse(v).Item1!).ToList(), true);
+                }
                 return (result.Select(v => (T)Convert.ChangeType(v, typeof(T))).ToList(), true);
             }
 
             return (null, false);
         }
+
     }
 }
