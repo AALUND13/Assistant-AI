b6bb8172d6166108201fc604898f221d592cc5a9
AALUND13 <tamlund1976@gmail.com>
2024-10-14 15:22:35 -0400

    Updated packages and added `VoiceNext` package.

--- Assistant.AI/Commands/Provider/OptionsProvider.cs
+++ Assistant.AI/Commands/Provider/OptionsProvider.cs
@@ -1,25 +1,21 @@
 ﻿using AssistantAI.AiModule.Attributes;
 using DSharpPlus.Commands.Processors.SlashCommands.ArgumentModifiers;
 using DSharpPlus.Commands.Trees;
+using DSharpPlus.Entities;
 using System.Reflection;
 
 namespace AssistantAI.Commands.Provider
 {
     internal class OptionsProvider : IChoiceProvider {
-        public ValueTask<IReadOnlyDictionary<string, object>> ProvideAsync(CommandParameter parameter) {
+        public ValueTask<IEnumerable<DiscordApplicationCommandOptionChoice>> ProvideAsync(CommandParameter parameter) {
             string[] optionNames = typeof(GuildOptions)
                 .GetProperties()
                 .Where(x => x.GetCustomAttribute<IgnoreAttribute>() == null)
                 .Select(x => x.Name)
                 .ToArray();
 
-            var options = new Dictionary<string, object>();
 
-            for(int i = 0; i < optionNames.Length; i++) {
-                options.Add(optionNames[i], optionNames[i]);
-            }
-
-            return new ValueTask<IReadOnlyDictionary<string, object>>(options);
+            return ValueTask.FromResult(optionNames.Select(optionName => new DiscordApplicationCommandOptionChoice(optionName, optionName)));
         }
     }
 }
