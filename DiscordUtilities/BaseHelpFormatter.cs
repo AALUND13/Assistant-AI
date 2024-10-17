5258f9873f2b99f9e426680983c732d6f9cef083
AALUND13 <tamlund1976@gmail.com>
2024-10-17 18:42:53 -0400

    Added `BaseHelpFormatter` abstract class.

---
+++ DiscordUtilities/BaseHelpFormatter.cs
@@ -0,0 +1,29 @@
+﻿using AssistantAI.DiscordUtilities.EventArgs;
+using DSharpPlus.Commands;
+using DSharpPlus.Commands.Trees;
+using DSharpPlus.Entities;
+
+namespace AssistantAI.DiscordUtilities {
+    public abstract class BaseHelpFormatter {
+        private static DiscordButtonComponent[] buttons = [
+            new DiscordButtonComponent(DiscordButtonStyle.Primary, "previous-category", "Previous Category"),
+            new DiscordButtonComponent(DiscordButtonStyle.Primary, "next-category", "Next Category")
+        ];
+
+        public abstract IEnumerable<DiscordMessageBuilder> FormatHelpMessage(Dictionary<string, IEnumerable<Command>> commandCategories, HelpFormatterEventArgs args);
+        public abstract Dictionary<string, IEnumerable<Command>> GetCommandCategories(IEnumerable<Command> commands);
+
+        protected DiscordMessageBuilder BuildHelpMessages(CommandsExtension commandsExtension, HelpFormatterEventArgs args) {
+            Dictionary<string, IEnumerable<Command>> commandCategories = GetCommandCategories(commandsExtension.Commands.Values);
+            IEnumerable<DiscordMessageBuilder> helpMessages = FormatHelpMessage(commandCategories, args);
+
+            DiscordMessageBuilder messageBuilder = new DiscordMessageBuilder();
+            foreach(var helpMessage in helpMessages) {
+                messageBuilder.AddComponents(buttons);
+                messageBuilder.AddEmbed(helpMessage.Embeds[args.CategoryIndex]);
+            }
+
+            return messageBuilder;
+        }
+    }
+}
---
+++ DiscordUtilities/EventArgs/HelpFormatterEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using DSharpPlus.Commands;
+using DSharpPlus.Entities;
+
+namespace AssistantAI.DiscordUtilities.EventArgs {
+    public class HelpFormatterEventArgs : System.EventArgs {
+        DiscordGuild? Guild { get; init; }
+        DiscordChannel? Channel { get; init; }
+        DiscordMessage? Message { get; init; }
+        DiscordInteraction? Interaction { get; init; }
+        CommandsExtension? CommandsExtension { get; init; }
+
+        public int CategoryIndex { get; set; }
+    }
+}
